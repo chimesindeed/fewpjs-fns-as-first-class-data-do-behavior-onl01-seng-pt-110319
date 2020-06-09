ec1b28f4731566763e527eec4a8f79c0503d84b5
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-09 06:21:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
 
-function greet(){}
+function greet(){
+  const hour = timeString.parseInt
+}
 function displayMessage(){}
