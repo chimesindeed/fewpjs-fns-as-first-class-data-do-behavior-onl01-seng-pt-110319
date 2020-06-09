adb375afe41a509e6b3818f1bff4492359d3c123
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-09 06:26:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,9 @@ function handleClick(e) {
 
 function greet(time){
   const hour = parseInt(time, 10);
+  if ( hour < 12) return "Good Morning";
+  if ( hour > 17) return "Good Evening";
+  return "Good Afternoon";
 }
+
 function displayMessage(){}
